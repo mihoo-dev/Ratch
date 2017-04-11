5cebd25b50a50e34366eb8a0761ace4395853a9f
hmha4 <4Makarl@gmail.com>
2017-04-12 00:10:04 +0900

    Ratch

---
+++ Assets/Script/FirstPersonPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonPlayer : MonoBehaviour {
+
+    public bool Alive = true;
+
+    void OnTriggerEnter(Collider Other)
+    {
+        if (Other.gameObject.name == "eyes")
+        {
+            Other.transform.parent.GetComponent<monster>().CheckSight();
+        }
+    }
+}
