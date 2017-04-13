2349b8269f997ba4d9bacc178a05a314c834c062
hmha4 <4Makarl@gmail.com>
2017-04-13 23:47:15 +0900

    Pause Button & Menu

---
+++ Assets/Script/PauseController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour {
+
+    public Transform canvas;
+	
+	// Update is called once per frame
+	void Update () {
+        Pause();
+	}
+
+    public void Pause()
+    {
+        if (canvas.gameObject.activeInHierarchy == false)
+        {
+            canvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            canvas.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
